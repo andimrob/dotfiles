616eb98f5989d407cc30e59eed31709310072088
Rob Blakey <andimrob@gmail.com>
2020-04-19 17:44:01 -0400

    feat: bunch of updates

--- bashrc.sh
+++ bashrc.sh
@@ -3,6 +3,3 @@
 # | '_ \ / _` / __| '_ \| '__/ __|
 # | |_) | (_| \__ \ | | | | | (__
 # |_.__/ \__,_|___/_| |_|_|  \___|
-
-export NVM_DIR="/Users/Rob/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
