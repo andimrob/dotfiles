c55a05a7b2db4e18196a9344ccf2b3a53dea3021
Rob Blakey <andimrob@gmail.com>
2021-10-10 23:36:31 -0400

    nvm

--- zshrc.sh
+++ zshrc.sh
@@ -107,3 +107,6 @@ unset file;
 eval "$(direnv hook zsh)"
 
 export PATH="$HOME/.poetry/bin:$PATH"
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
