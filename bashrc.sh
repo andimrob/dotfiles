d0da6e490cdfefdb8816d23ca036bb313fb2ee4b
Robert White <robert.emery1@gmail.com>
2021-10-10 23:36:31 -0400

    nvm

--- bashrc.sh
+++ bashrc.sh
@@ -3,3 +3,7 @@
 # | '_ \ / _` / __| '_ \| '__/ __|
 # | |_) | (_| \__ \ | | | | | (__
 # |_.__/ \__,_|___/_| |_|_|  \___|
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
