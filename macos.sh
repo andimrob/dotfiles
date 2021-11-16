7d53cb41873fca8e75ac2a894ee89c3e1c93445e
Rob Blakey <andimrob@gmail.com>
2021-11-16 14:55:40 -0500

    feat: fast key repeat

--- macos.sh
+++ macos.sh
@@ -13,3 +13,7 @@ alias hidefiles='defaults write com.apple.finder AppleShowAllFiles FALSE && kill
 alias showfiles='defaults write com.apple.finder AppleShowAllFiles TRUE && killall Finder'
 
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write NSGlobalDomain InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
