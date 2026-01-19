1ac517a6826256709a60882ad1ea0534d626f7f8
Robert Blakey <robert.emery1@gmail.com>
2026-01-19 13:21:50 -0500

    chore: delete everything

--- .claude/CLAUDE.md
+++
@@ -1,7 +0,0 @@
-# Commit Messages
-- Always write git commits using conventional commit style
-- Limit git messages to 60 characters
-- Never write attribution to Claude
-
-# Github
-When creating PRs never write attributions to Claude
--- bashrc.sh
+++
@@ -1,9 +0,0 @@
-#  _               _
-# | |__   __ _ ___| |__  _ __ ___
-# | '_ \ / _` / __| '_ \| '__/ __|
-# | |_) | (_| \__ \ | | | | | (__
-# |_.__/ \__,_|___/_| |_|_|  \___|
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
